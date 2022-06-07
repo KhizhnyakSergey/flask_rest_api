1db6159789541caa9a503522c50ee02e6cdf7ff7
KhizhnyakSergey <Khihnyak56843@gmail.com>
2022-06-07 20:59:50 +0300

    feat: delete record method

--- local.py
+++ local.py
@@ -1,4 +1,7 @@
 import requests
 
-res = requests.get("http://127.0.0.1:3000/api/courses/0")
+res = requests.delete("http://127.0.0.1:3000/api/courses/1")
 print(res.json())
+
+# single entry display
+# res = requests.get("http://127.0.0.1:3000/api/courses/1")
