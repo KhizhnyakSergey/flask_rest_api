e08029a4227904597569d3940b5985f7cafb6408
KhizhnyakSergey <Khihnyak56843@gmail.com>
2022-06-07 20:53:29 +0300

    feat: single entry display

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api()
+
+courses = {
+    1: {'name': 'Python', 'videos': 15},
+    2: {'name': 'Java', 'videos': 10}
+}
+
+
+class Main(Resource):
+    def get(self, course_id):
+        if course_id == 0:
+            return courses
+        else:
+            return courses[course_id]
+
+
+api.add_resource(Main, "/api/courses/<int:course_id>")
+api.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000, host="127.0.0.1")
